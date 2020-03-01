5883e43eb0bf0ca8927fd0bbad716cbfd44b6b90
Ítalo Otávio <italodxd@gmail.com>
2020-03-01 17:24:49 -0300

    Init Project

---
+++ app/src/main/java/com/app/androidfundamentals13/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.androidfundamentals13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
